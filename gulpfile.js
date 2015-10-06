f9f23313a1b32ae6b46b96c2abc3bf4e7fe8d338
Mudi Ugbowanko <mudi@renegare.com>
2015-10-06 03:55:04 +0100

    add tutum api, to blue green deploy. missing elb

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp-help')(require('gulp')),
     gulpUtil = require('gulp-util'),
-    tutum = require('tutum'),
+    Tutum = require('tutum'),
     Prom = require('bluebird'),
     sh = Prom.promisify(require('shelljs').exec),
     path = require('path'),
@@ -42,10 +42,79 @@ gulp.task('push', function() {
 //@todo: should use the tutum api
 gulp.task('deploy', function() {
     return grev().then(function(rev) {
-        return sh([
-                ['tutum login -u', TUSER, '-p', TPASS].join(' '),
-                ['tutum service terminate ', APP].join(''),
-                ['tutum service run -p 80:80 -n ', APP, ' tutum.co/', TUSER,'/', IMAGE, ':', rev].join('')
-            ].join(' && '));
-    })
+        // login
+        var tutum = Prom.promisifyAll(new Tutum({
+                username: process.env.TUSER,
+                apiKey: process.env.TAPIKEY
+            }));
+
+            function validateService(service, target) {
+                if(service && ['not running', 'running'].indexOf(service.state.toLowerCase()) === -1) {
+                    throw new Error(['Invalid service state:', target, service.state].join(' '));
+                }
+            }
+
+            function terminateService(service) {
+                console.log('Terminating:', service.name);
+                return tutum.deleteAsync('service/' + service.uuid);
+            }
+
+            return Prom.join(tutum.getAsync('service', {name: APP + '-blue'}), tutum.getAsync('service', {name: APP + '-green'}))
+                .spread(function(blue, green) {
+                    return [
+                        blue.objects.filter(function(service) {
+                            return service.state !== 'Terminated';
+                        }).pop(),
+                        green.objects.filter(function(service) {
+                            return service.state !== 'Terminated';
+                        }).pop()
+                    ]
+                })
+                .spread(function(blue, green) {
+                    validateService(blue, APP + '-blue');
+                    validateService(green, APP + '-green');
+                    return [blue, green];
+                })
+                .spread(function(blue, green) {
+                    // get target service name (e.g. green or blue)
+                    return [
+                        APP + '-' + (blue? 'green' : 'blue'),
+                        blue? green : blue,
+                        blue? blue : green
+                    ];
+                })
+                .spread(function(target, previous, current) {
+                    if(previous && previous.state !== 'Terminated') {
+                        previous = terminateService(previous);
+                    }
+                    return [target, current, previous];
+                })
+                .delay(1000)
+                .spread(function(target, current) {
+                    // launch target service
+                    console.log('Deploying target:', target);
+                    return Prom.join(tutum.postAsync('service', {
+                            image: ['tutum.co/', TUSER,'/', IMAGE, ':', rev].join(''),
+                            name: target,
+                            container_ports: [{"protocol": "tcp", "inner_port": 80, "outer_port": 80, "published": true}],
+                            autorestart: 'ALWAYS',
+                            // tags: ['renegare']
+                        }), current);
+                })
+                .delay(1000)
+                .spread(function(target, current) {
+                    return [tutum.postAsync('service/' + target.uuid + '/start'), current];
+                })
+                .delay(1000)
+                .spread(function(target, current) {
+                    // @todo: wait until lb has updated
+
+                    // terminate old services
+                    return terminateService(current);
+                })
+                .catch(function(err) {
+                    console.log('Error: ', err.text || err);
+                    throw err;
+                })
+    });
 });
